90625ecf71611fa4fcb9ccda0582f77a2914b63b
Adam Sparks <adspar@gmail.com>
2016-10-27 11:48:04 -0700

    Add files via upload

--- datasquadassignment1.R
+++ datasquadassignment1.R
@@ -1,1 +1,11 @@
+# import data
 raw1 = read.csv("RAW DATA game1_results FINAL.csv", header=TRUE)
+# the line above can be slightly modified to read in the other files, e.g.
+raw2 = read.csv("RAW DATA game2_results FINAL.csv", header=TRUE)
+# repeat for all of the raw data
+
+
+# next we'll start merging. in the 2 files included above, the variable to use to merge will be "dg1"
+# there's a way to merge with base R, but either tidyR or dplyr would probably be better
+# also, eventually we won't want to load the full raw files (as above) -- probably we can just create the merged file as a new object without making an object for the raw file -- it can just read data from it
+
