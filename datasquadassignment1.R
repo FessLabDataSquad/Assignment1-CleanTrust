cd7113869da3f568b08ee72544069c148f7a21cf
kaialex94 <kai.yamamoto.alexander@gmail.com>
2016-11-17 00:34:39 -0800

    Update datasquadassignment1.R

--- datasquadassignment1.R
+++ datasquadassignment1.R
@@ -16,3 +16,18 @@ raw2 = read.csv("RAW DATA game2_results FINAL.csv", header=TRUE)
 
 full_clean_data = merge (( useful shit from "RAW DATA game1_result FINAL.csv", useful shit from 2, 3, etc, etc...))
 
+# This is one way to look for duplicate IDs (I found this on stackoverflow). 
+# Using the table() function will give you a list of all the IDs and the number of times they occured. 
+n_occur <- data.frame(table(raw1$dg1))
+# You can narrow the list down to IDs that appear more than once
+n_occur[n_occur$Freq > 1,]
+#This will show as: 
+              Var1 Freq
+24      1grape1230    2
+137     3grape1230    2
+273     5grape1230    2
+336 trialmorningc3    2
+#You can then obtain the records for each duplicated ID
+raw1[raw1$dg1 %in% n_occur$Var1[n_occur$Freq > 1],]
+
+
